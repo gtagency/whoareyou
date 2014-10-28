7f3a4daec50ceae7360a604f94a3e617f132603d
Robert Smith <smithrobertlawrence@gmail.com>
2014-10-28 19:08:21 -0400

    Made POSTagger customizable
    Split up Process_file into parts

--- POSTagger.py
+++ POSTagger.py
@@ -7,18 +7,36 @@ from nltk import *
 import operator
 from os import listdir
 from os.path import isfile, join
+import re
+import string
 
 #runs some nlp on the given file
 #returns the average sentence length and a sorted array of
 #POS/occurence-percentage pairs.
+def parse_sentences(filename):
+	contents = re.split("[.!?]", "".join([line.decode('utf-8').strip() for line in open(filename).readlines()]))
+	return contents
+def edit_data(filename):
+	contents = parse_sentences(filename)
+	final_contents = []
+	for sentence in contents:
+		#This is where you can choose what data to parse
+		if (sentence != ""):
+			last_word = sentence.split()[-1]
+		else:
+			last_word = sentence
+		final_contents.append(last_word)
+	return final_contents
+
+
+
 def process_file(filename):
-	contents = "".join([line.decode('utf-8').strip() for line in open(filename).readlines()]).split(".")
+	contents = edit_data(filename)
 	parts = {}
-	word_count = 0
+	word_count = count_words(contents)
 	for sentence in contents:
 		#this is the actual nltk magic
 		words = word_tokenize(sentence)
-		word_count += len(words)
 		#and here
 		poss = pos_tag(words)
 		for word, pos in poss:
@@ -28,6 +46,13 @@ def process_file(filename):
 	parts = [(a, round(b / word_count, 2)) for (a,b) in parts]
 	sent_avg = word_count / len(contents)
 	return (round(sent_avg, 2), parts)
+def count_words(contents):
+	word_count = 0
+	for sentence in contents:
+		words = sentence.split()
+		word_count += len(words)
+	return word_count
+
 
 # path = "./data/texts"
 # raw = "/raw"
@@ -37,7 +62,7 @@ def process_file(filename):
 #     result.write(str(process_file(path + raw + "/" + speech)))
 #print process_file("texts/BarrackObama.txt")
 #print process_file("RonaldReagan.txt")
-print process_file("./test/texts/JamesGarfield.txt")
+print process_file("./data/texts/raw/BillClinton.txt")
 
 # uncomment the below to see the POS codes
 # help.upenn_tagset()
